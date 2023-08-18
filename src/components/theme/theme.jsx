4b06820206e0a8427860c98274b18aa0639e944c
M-Ahmed-dev <winterfell551@gmail.com>
2023-08-18 19:44:13 +0500

    intial commit

--- src/components/theme/theme.jsx
+++ src/components/theme/theme.jsx
@@ -1,14 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  // styles: {
-  //   global: {
-  //     body: {
-  //       bg: "linearGradient(135deg, #fff 0%, #d0e8eb 100%)",
-  //       fontFamily: "Noto Sans TC, sans-serif",
-  //     },
-  //   },
-  // },
+  styles: {
+    global: {
+      body: {
+        bg: "linearGradient(135deg, #fff 0%, #d0e8eb 100%)",
+        fontFamily: "Noto Sans TC, sans-serif",
+      },
+    },
+  },
 
   colors: {
     primary: "#215B7C",
